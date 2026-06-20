74770b1d599b0d517a855f902fc4c51b019e7702
Abdeen59 <abdeen.aboda@gmail.com>
2026-06-20 17:08:43 +0300

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,10 @@
 
+const CONFIG = {
+  APP_NAME: "LuxBid",
+  APP_VERSION: "2.0",
+
+  SUPABASE_URL: "https://upkqptjkzxyntbvfjakg.supabase.co",
+  SUPABASE_ANON_KEY: "ضع هنا مفتاح Supabase Publishable Key",
+
+  PI_SANDBOX: false
+};
